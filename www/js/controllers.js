61905b2fd4e8fbc313ebe7c8fd5573e47be167cf
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-05-26 00:28:20 +0700

    feat: mock sembakos on every pages

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function ($scope, $ionicSideMenuDelegate) {
+.controller('DashCtrl', function ($scope, $ionicSideMenuDelegate, Sembakos) {
     $scope.toggleLeft = function() {
       $ionicSideMenuDelegate.toggleLeft();
     };
@@ -8,40 +8,34 @@ angular.module('starter.controllers', [])
         labels: [2, 5, 6, 9, 10, 11, 12, 13, 16, 17, 18, 20, 23, 24, 25, 26, 27],
         datasets: [
             {
-                label: "Minyak Goreng Kemasan",
-                fillColor: "rgba(220,220,220,0.2)",
-                strokeColor: "rgba(220,220,220,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [33253,30584,30356,29284,28674,28535,28263,28851,28073,27322,27281,27222,25948,25674,24639,24485,24587]
-            },
-            {
-                label: "Minyak Goreng Kemasan",
                 fillColor: "rgba(220,0,0,0.2)",
                 strokeColor: "rgba(220,0,0,1)",
                 pointColor: "rgba(220,220,220,1)",
                 pointStrokeColor: "#fff",
                 pointHighlightFill: "#fff",
                 pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [30000,null,null,null,null,28035,28263,28851,28073,27322,27281,27222,25948,25674,24639,24485,24587]
+                data: [33253,30584,30356,29284,28674,28535,28263,28851,28073,27322,27281,27222,25948,25674,24639,24485,24587]
             },
         ]
     };
     var ctx = document.getElementById("myChart").getContext("2d");
     var myLineChart = new Chart(ctx).Line(data);
+
+    $scope.sembakos = Sembakos.all();
+    console.log( $scope.sembakos )
 })
 
-.controller('PantauansCtrl', function($scope, Pantauans) {
+.controller('PantauansCtrl', function($scope, Pantauans, Sembakos) {
   $scope.pantauans = Pantauans.all();
   $scope.remove = function(pantauan) {
     Pantauans.remove(pantauan);
   }
+  $scope.sembakos = Sembakos.all();
 })
 
-.controller('PantauanDetailCtrl', function($scope, $stateParams, Pantauans) {
+.controller('PantauanDetailCtrl', function($scope, $stateParams, Pantauans, Sembakos) {
   $scope.pantauan = Pantauans.get($stateParams.pantauanId);
+  $scope.sembakos = Sembakos.all();
 })
 
 .controller('AccountCtrl', function($scope) {
