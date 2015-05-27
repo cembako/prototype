407c17f345ea3f1b8e653f84c8be7b9778e647be
Rizky Luthfianto <mrluthfianto@gmail.com>
2015-05-27 18:24:43 +0700

    feat: chart, tapi fail

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,45 +1,63 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function ($scope, $ionicSideMenuDelegate, Sembakos) {
-    $scope.toggleLeft = function() {
+.controller('DashCtrl', function($scope, $ionicSideMenuDelegate, Sembakos) {
+  $scope.toggleLeft = function() {
       $ionicSideMenuDelegate.toggleLeft();
-    };
+  };
+
+  $scope.sembakos = Sembakos.all();
+  console.log($scope.sembakos)
+
+  $scope.selectedSembako=$scope.sembakos[2]
+  console.log('di controller, di luar drawChart')
+
+  $scope.drawChart = function() {
+    console.log('drawChart')
+    console.log($scope.selectedSembako)
+    
     var data = {
         labels: [2, 5, 6, 9, 10, 11, 12, 13, 16, 17, 18, 20, 23, 24, 25, 26, 27],
-        datasets: [
-            {
-                fillColor: "rgba(220,0,0,0.2)",
-                strokeColor: "rgba(220,0,0,1)",
-                pointColor: "rgba(220,220,220,1)",
-                pointStrokeColor: "#fff",
-                pointHighlightFill: "#fff",
-                pointHighlightStroke: "rgba(220,220,220,1)",
-                data: [33253,30584,30356,29284,28674,28535,28263,28851,28073,27322,27281,27222,25948,25674,24639,24485,24587]
-            },
-        ]
+        datasets: [{
+          fillColor: "rgba(220,0,0,0.2)",
+          strokeColor: "rgba(220,0,0,1)",
+          pointColor: "rgba(220,220,220,1)",
+          pointStrokeColor: "#fff",
+          pointHighlightFill: "#fff",
+          pointHighlightStroke: "rgba(220,220,220,1)",
+          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+      }]
     };
+
+
+    var dataArray = []
+    for(var o in $scope.selectedSembako.prices) 
+      dataArray.push($scope.selectedSembako.prices[o]);
+    data.datasets[0].data=dataArray;
+    console.log(data.datasets[0].data)
+
     var ctx = document.getElementById("myChart").getContext("2d");
     var myLineChart = new Chart(ctx).Line(data);
+  }
 
-    $scope.sembakos = Sembakos.all();
-    console.log( $scope.sembakos )
+  $scope.drawChart();
+
 })
 
 .controller('PantauansCtrl', function($scope, Pantauans, Sembakos) {
-  $scope.pantauans = Pantauans.all();
-  $scope.remove = function(pantauan) {
-    Pantauans.remove(pantauan);
-  }
-  $scope.sembakos = Sembakos.all();
+    $scope.pantauans = Pantauans.all();
+    $scope.remove = function(pantauan) {
+        Pantauans.remove(pantauan);
+    }
+    $scope.sembakos = Sembakos.all();
 })
 
 .controller('PantauanDetailCtrl', function($scope, $stateParams, Pantauans, Sembakos) {
-  $scope.pantauan = Pantauans.get($stateParams.pantauanId);
-  $scope.sembakos = Sembakos.all();
+    $scope.pantauan = Pantauans.get($stateParams.pantauanId);
+    $scope.sembakos = Sembakos.all();
 })
 
 .controller('AccountCtrl', function($scope) {
-  $scope.settings = {
-    enableFriends: true
-  };
+    $scope.settings = {
+        enableFriends: true
+    };
 });
